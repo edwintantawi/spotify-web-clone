f3bab8fd9b0dfff2be8f63af807d496c442e8e47
edwintantawi <edwintantawi.workspace@gmail.com>
2022-10-04 01:14:59 +0700

    feat: add Logo component (spotify svg logo)

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,12 @@
+import { Logo } from '~/components/Logo';
+
 function App() {
   return (
-    <div className="bg-spotify grid h-screen place-content-center bg-primary">
-      <h1 className="text-4xl font-bold text-black">Spotify</h1>
+    <div className="grid h-screen place-content-center bg-primary">
+      <div>
+        <Logo invert className="mx-auto mb-2 w-16" />
+        <h1 className="text-4xl font-bold text-black">Spotify</h1>
+      </div>
     </div>
   );
 }
